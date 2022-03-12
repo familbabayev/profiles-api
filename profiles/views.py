8db8be9685ac3fe0fb80e9676d6e9f77acbd5b16
familhacker <familhacker@gmail.com>
2022-03-12 20:20:39 +0400

    created simple ViewSet with all methods

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, viewsets
 
 from .serializers import HelloSerializer
 
@@ -8,14 +8,13 @@ class HelloApiView(APIView):
     serializer_class = HelloSerializer
 
     def get(self, request, format=None):
-
         an_apiview = [
             'Uses HTTP methods as function (get, post, patch, put, delete)'
             'Gives you most control over your application logic'
         ]
         
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
-        # return Response()
+
 
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
@@ -44,4 +43,44 @@ class HelloApiView(APIView):
         return Response({'method':'DELETE'})
 
 
+class HelloViewSet(viewsets.ViewSet):
+    serializer_class = HelloSerializer
+
+    def list(self, request):
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides mode functionality with less code'
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+    def retrieve(self, request, pk=None):
+        return Response({'method':'GET'})
     
+    
+    def update(self, request, pk=None):
+        return Response({'method':'PUT'})
+
+
+    def partial_update(self, request, pk=None):
+        return Response({'method':'PATCH'})
+
+
+    def destroy(self, request, pk=None):
+        return Response({'method':'DELETE'})
